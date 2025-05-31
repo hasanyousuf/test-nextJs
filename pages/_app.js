42c4bff99bb3e113376b863a792cde4d9c995c5b
Hasan Naqvi <hasany@gmail.com>
2025-05-31 08:39:12 +0100

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-        <h1>Header</h1>
+        <h1 bgcolor="#aabbcc">Header</h1>
 
   <Component {...pageProps} />
     <footer>My footer</footer>
